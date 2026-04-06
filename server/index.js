23f9ed08c5bcb074598ff75ba0cd0644557bf1a2
Lei Lei <tlei@haverford.edu>
2026-04-06 01:17:02 -0700

    backend connection config

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ app.use(cors({
     const allowedOrigins = [
       'http://localhost:5173',
       'http://localhost:3001',
+      'https://haggle-ai.org',
       process.env.FRONTEND_URL,
     ].filter(Boolean)
     
